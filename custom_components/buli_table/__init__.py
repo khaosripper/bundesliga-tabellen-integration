f63f3f7dddb4fc9aa795dd1db0451ce5d32174b2
KhaosRipper <65299946+KhaosRipper@users.noreply.github.com>
2026-05-23 15:27:45 +0200

    Create __init__.py

---
+++ custom_components/buli_table/__init__.py
@@ -0,0 +1,11 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
