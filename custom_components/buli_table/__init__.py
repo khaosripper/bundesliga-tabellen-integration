fef0a51c5b9c2f066df01cdf78e2c70f63368bc6
KhaosRipper <65299946+KhaosRipper@users.noreply.github.com>
2026-05-23 16:07:05 +0200

    Update __init__.py

--- custom_components/buli_table/__init__.py
+++ custom_components/buli_table/__init__.py
@@ -1,9 +1,19 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.components.frontend import add_extra_js_url
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
+    
+    hass.http.register_static_path(
+        "/buli_table/buli-table-card.js",
+        hass.config.path("custom_components/buli_table/buli-table-card.js"),
+        False
+    )
+    
+    add_extra_js_url(hass, "/buli_table/buli-table-card.js")
+
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
